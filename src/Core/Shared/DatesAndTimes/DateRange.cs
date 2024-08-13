30d4b2601af4ea69983d48698b7dfe40b227f3a5
LaBani Said <labani.said@outlook.com>
2024-08-13 21:09:11 +0200

    Add DateRange.cs

---
+++ src/Core/Shared/DatesAndTimes/DateRange.cs
@@ -0,0 +1,9 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="DateRange.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Core.Shared.DatesAndTimes;
+
+public record struct DateRange(DateOnly StartDate, DateOnly EndDate);
