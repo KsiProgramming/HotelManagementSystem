bcb91fc06651fba340da151083ebfcbf436c7eb3
LaBani Said <labani.said@outlook.com>
2024-08-14 13:26:42 +0200

    Add AvailableRoomTypeIds.cs to Core

---
+++ src/Core/Availability/Domain/AvailableRoomTypeIds.cs
@@ -0,0 +1,13 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AvailableRoomTypeIds.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Availability;
+using System.Collections.ObjectModel;
+
+public class AvailableRoomTypeIds
+{
+    public Collection<int> AvailableRoomIds { get; } = new Collection<int>();
+}
