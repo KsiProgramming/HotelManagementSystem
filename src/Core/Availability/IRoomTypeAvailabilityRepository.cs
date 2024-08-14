a7c4b833c9c95d956be3fb13fe8981b7a7bf1c75
LaBani Said <labani.said@outlook.com>
2024-08-14 14:25:59 +0200

    Add IRoomTypeAvailabilityRepository to Core

---
+++ src/Core/Availability/IRoomTypeAvailabilityRepository.cs
@@ -0,0 +1,12 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IRoomTypeAvailabilityRepository.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Availability;
+
+public interface IRoomTypeAvailabilityRepository
+{
+    public IReadOnlyList<int> FindAvailableRoomTypeIds(RoomTypeAvailibilityQuery query);
+}
