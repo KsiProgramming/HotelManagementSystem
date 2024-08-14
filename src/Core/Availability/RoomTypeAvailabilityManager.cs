27036136ea559fc3f67c8995c8fa116070825248
LaBani Said <labani.said@outlook.com>
2024-08-14 14:29:05 +0200

    Add RoomTypeAvailabilityManager.cs to Core with implemantation of FindAvailableRoomTypeIds

---
+++ src/Core/Availability/RoomTypeAvailabilityManager.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RoomTypeAvailabilityManager.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Availability;
+
+public class RoomTypeAvailabilityManager
+{
+    private readonly IRoomTypeAvailabilityRepository repository;
+
+    public RoomTypeAvailabilityManager(IRoomTypeAvailabilityRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public IReadOnlyList<int> FindAvailableRoomTypeIds(RoomTypeAvailibilityQuery query)
+    {
+        return this.repository.FindAvailableRoomTypeIds(query);
+    }
+}
