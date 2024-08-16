2ab7307ad588fc8d1ffbae0fefd4d706d075bde9
LaBani Said <labani.said@outlook.com>
2024-08-16 11:13:13 +0200

    Add GuestForCreation.cs in Core

---
+++ src/Core/Guests/GuestForCreation.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GuestForCreation.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Guests;
+
+public class GuestForCreation
+{
+    required public string ReservationUuid { get; init; }
+
+    required public Guest Guest { get; init; }
+}
