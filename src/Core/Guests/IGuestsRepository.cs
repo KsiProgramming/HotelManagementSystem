775234f2a62ddfa30cda0c0c48587d82f4ab49c9
LaBani Said <labani.said@outlook.com>
2024-08-16 11:01:54 +0200

    Add IGuestsRepository.cs in Core

---
+++ src/Core/Guests/IGuestsRepository.cs
@@ -0,0 +1,12 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IGuestsRepository.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Guests;
+
+public interface IGuestsRepository
+{
+    public Task<IReadOnlyList<Guest>> Find(GuestsQuery query);
+}
