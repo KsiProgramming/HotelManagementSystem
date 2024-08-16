aa2646f899ea9a3a3538a8d7d714cd40d8b1e78d
LaBani Said <labani.said@outlook.com>
2024-08-16 10:59:45 +0200

    Add GuestsQuery.cs in Core

---
+++ src/Core/Guests/GuestsQuery.cs
@@ -0,0 +1,14 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GuestsQuery.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Guests;
+
+using System.Collections.ObjectModel;
+
+public class GuestsQuery
+{
+    public Collection<int> Ids { get; } = new Collection<int>();
+}
