2140839dd7e302ff39b7b599b812608425cec726
LaBani Said <labani.said@outlook.com>
2024-08-16 10:56:42 +0200

    Add Guest.cs in Core

---
+++ src/Core/Guests/Domain/Guest.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="Guest.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Guests;
+
+public class Guest
+{
+    required public string Id { get; init; }
+
+    required public string Title { get; init; }
+
+    required public string FirstName { get; init; }
+
+    required public string LastName { get; init; }
+
+    required public string Email { get; init; }
+
+    required public string Address { get; init; }
+}
