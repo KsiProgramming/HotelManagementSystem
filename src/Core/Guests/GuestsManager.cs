7dc6b45331f17f2020aebd01bbfcaff7f52ef6af
LaBani Said <labani.said@outlook.com>
2024-08-16 11:08:05 +0200

    Add GuestsManager.cs to Core

---
+++ src/Core/Guests/GuestsManager.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GuestsManager.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Guests;
+
+public class GuestsManager
+{
+    private readonly IGuestsRepository repository;
+
+    public GuestsManager(IGuestsRepository repository) => this.repository = repository;
+
+    public async Task<IReadOnlyList<Guest>> FindAsync(GuestsQuery query) => await this.repository.Find(query);
+}
