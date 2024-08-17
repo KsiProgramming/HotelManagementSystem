a3bdadc17a05bf73576aa1c8bee0637b85b15532
LaBani Said <labani.said@outlook.com>
2024-08-17 18:58:55 +0200

    Add IRoomTypeCollateralRepository.cs to Marketing Core

---
+++ src/Core/Marketing/IRoomTypeCollateralRepository.cs
@@ -0,0 +1,12 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="IRoomTypeCollateralRepository.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Marketing;
+
+public interface IRoomTypeCollateralRepository
+{
+    public Task<IReadOnlyList<RoomTypeCollateral>> FindAsync(RoomTypeCollateralQuery query);
+}
