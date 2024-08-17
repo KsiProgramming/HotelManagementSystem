24b6ce555ffb3cd67982ae2d550d5507deb96eac
LaBani Said <labani.said@outlook.com>
2024-08-17 19:01:35 +0200

    Add RoomTypeCollateralManager.cs to Marketing Core

---
+++ src/Core/Marketing/RoomTypeCollateralManager.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RoomTypeCollateralManager.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Marketing;
+
+public class RoomTypeCollateralManager
+{
+    private readonly IRoomTypeCollateralRepository repository;
+
+    public RoomTypeCollateralManager(IRoomTypeCollateralRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public async Task<IReadOnlyList<RoomTypeCollateral>> FindAsync(RoomTypeCollateralQuery query)
+    {
+        return await this.repository.FindAsync(query);
+    }
+}
