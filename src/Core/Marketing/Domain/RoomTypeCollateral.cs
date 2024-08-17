87c8d99f97d3a446132a2a562f3334f6f68a56de
LaBani Said <labani.said@outlook.com>
2024-08-17 18:54:09 +0200

    Add RoomTypeCollateral.cs to Marketing

---
+++ src/Core/Marketing/Domain/RoomTypeCollateral.cs
@@ -0,0 +1,16 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RoomTypeCollateral.cs" company="HotelManagementSystem">
+//     Copyright (c) HotelManagementSystem. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace HotelManagementSystem.Marketing;
+
+public class RoomTypeCollateral
+{
+    required public int RoomTypeId { get; init; }
+
+    required public string Description { get; init; }
+
+    required public string ImageUri { get; init; }
+}
